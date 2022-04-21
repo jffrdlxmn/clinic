a171fe0427fb1ad87e05aa90464ba352c6455de1
Jeffrid Laxamana <jeffrid.laxamana@cvsu.edu.ph>
2022-04-21 11:37:23 +0800

    change title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport"  http-equiv="Content-Type" content="width=device-width, initial-scale=1">
-  <title>MIS COFFEE</title>
+  <title>Supply</title>
   
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
